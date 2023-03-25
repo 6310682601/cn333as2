9baf0d372d0a9db462715206ccd901ffa467e5fb
munin <6310682601@student.tu.ac.th>
2023-03-25 22:25:34 +0700

    create app

---
+++ app/src/main/java/com/example/quiz_game/data/QuestionData.kt
@@ -0,0 +1,40 @@
+package com.example.quiz_game.data
+
+val allQuestion = listOf(
+    "What can you catch but not throw?",
+    "What has a face and two hands but no arms?",
+    "What can run but never walks, has a mouth but never talks, has a head but never weeps, has a bed but never sleeps?",
+    "What has a neck but no head?",
+    "What has teeth but cannot bite?",
+    "What is it that you must keep after giving it to someone?",
+    "What is as light as a feather, but even the strongest man cannot hold it for much more than a minute?",
+    "They come out at night without being called and are lost in the day without being stolen. What are they?",
+    "What has been around for millions of years but is no more than a month old?",
+    "No sooner spoken than broken. What is it?"
+)
+
+val allAnswer = listOf(
+    listOf("a cold","a ball","a bird","a bouquet"),
+    listOf("a clock","a mirror","a playing card","a radio"),
+    listOf("a river","a nose","a tree","a wolf"),
+    listOf("a bottle","a comet","a flower","a snake"),
+    listOf("a comb","a bee","a puppy","a toothpick"),
+    listOf("your word","your pen","your money","your time"),
+    listOf("breath","air","fire","light"),
+    listOf("stars","cockroaches","nightmares","robbers"),
+    listOf("the moon","the sun","the universe","the wind"),
+    listOf("silence","friendship","glass","news"),
+)
+
+val trueAnswer = listOf(
+    "a cold",
+    "a clock",
+    "a river",
+    "a bottle",
+    "a comb",
+    "your word",
+    "breath",
+    "stars",
+    "the moon",
+    "silence"
+)
