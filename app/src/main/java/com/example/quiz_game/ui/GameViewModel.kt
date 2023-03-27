7973f684a1e7425c6eaeec49a25b5e23eb8f99c6
munin <6310682601@student.tu.ac.th>
2023-03-27 23:20:01 +0700

    edit all

--- app/src/main/java/com/example/quiz_game/ui/GameViewModel.kt
+++ app/src/main/java/com/example/quiz_game/ui/GameViewModel.kt
@@ -1,21 +1,11 @@
 package com.example.quiz_game.ui
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.quiz_game.data.MAX_Questions
-import com.example.quiz_game.data.SCORE_INCREASE
-import com.example.quiz_game.data.allAnswer
-import com.example.quiz_game.data.allQuestion
-import com.example.quiz_game.data.trueAnswer
+import com.example.quiz_game.data.Question
+import com.example.quiz_game.data.questions
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import java.lang.Math.random
-import kotlin.random.Random
-import kotlin.random.Random.Default.nextInt
 
 
 class GameViewModel : ViewModel() {
@@ -23,100 +13,41 @@ class GameViewModel : ViewModel() {
 //    private val Question = Question()
 //    var currentQuestionIndex = Question.question.shuffled()
 //    var index = Random.nextInt(0,9)
-    private val _uiState = MutableStateFlow(GameUiState())
+    var question = questions.shuffled()
+    var score = 0
+    var quizIndex = 0
+
+    private val _uiState = MutableStateFlow(GameUiState(
+        currentQuestion = question[quizIndex],
+        choice = question[quizIndex].choice.shuffled(),
+        score = 0,
+        quizIndex = 0
+    ))
+
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
 
-    var userGuess by mutableStateOf("")
-        private set
 
-    // Set of words used in the game
-    private var usedQuestion: MutableSet<Int> = mutableSetOf()
-    private lateinit var currentQuestion: String
-
-    init {
-        resetGame()
-    }
-    fun resetGame() {
-        usedQuestion.clear()
-//        _uiState.value = GameUiState(currentScrambledQuestion = pickRandomQuestion())
-    }
-
-    fun updateUserGuess(guessedWord: String){
-        userGuess = guessedWord
-    }
-
-    /*
-     * Checks if the user's guess is correct.
-     * Increases the score accordingly.
-     */
-    fun checkAns() {
-
-//        val correctAnswers = trueAnswer[currentQuestionIndex]
-
-        if (userGuess in trueAnswer) {
-            val updatedScore = _uiState.value.score.plus(SCORE_INCREASE)
-            updateGameState(updatedScore)
+    fun getQuestion(): Question {
+        if (quizIndex != 9) {
+            quizIndex += 1
+            return question[quizIndex]
         } else {
-            _uiState.update { currentState ->
-                currentState.copy(isWrong = true)
-            }
-        }
-        // Reset user guess
-        updateUserGuess("")
-    }
-
-    private fun updateGameState(updatedScore: Int) {
-        if (usedQuestion.size == MAX_Questions){
-            _uiState.update { currentState ->
-                currentState.copy(
-                    isWrong = false,
-                    score = updatedScore,
-                    isGameOver = true
-                )
-            }
-        } else{
-            // Normal round in the game
-            _uiState.update { currentState ->
-                currentState.copy(
-                    isWrong = false,
-//                    currentScrambledQuestion = pickRandomQuestion(),
-                    currentQuestionCount = currentState.currentQuestionCount.inc(),
-                    score = updatedScore
-                )
-            }
+            return question[quizIndex]
         }
     }
 
+    fun checkAnswer( input: String) {
+        if (input == question[quizIndex].answer) {
+            score += 1
+        } else {
+            score += 0
+        }
+    }
 
-//    fun pickRandomQuestion(): Pair<String, List<String>> {
-//        var randomQuestion = allQuestion.random()
-//        while (usedQuestion.contains(randomQuestion)) {
-//            randomQuestion = allQuestion.random()
-//        }
-//        usedQuestion.add(randomQuestion)
-//        val index = allQuestion.indexOf(randomQuestion)
-//        val answers = allAnswer[index]
-//        return Pair(randomQuestion, answers)
-//    }
-    fun pickRandomQuestion(): List<String> {
-        var index = Random.nextInt(0,9)
-//        if (index in usedQuestion){
-//            index = Random.nextInt(0,9)
-//            pickRandomQuestion()
-//        }
-//        else{
-//            usedQuestion.add(index)
-//        }
-
-        var randomQuestion = allQuestion[index]
-        val answers = allAnswer[index].shuffled()
-        val ans1 = answers[0]
-        val ans2 = answers[1]
-        val ans3 = answers[2]
-        val ans4 = answers[3]
-        val output = listOf(randomQuestion, ans1, ans2, ans3, ans4)
-
-        return output
+    fun reset() {
+        score = 0
+        quizIndex = 0
     }
 
 }
+
